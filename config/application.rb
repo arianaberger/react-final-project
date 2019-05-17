0c2232f247b23ec96bb431faecd8d9e48f56e73a
arianaberger <ariana.berger@gmail.com>
2019-05-17 18:27:01 -0400

    Remove code for active storage

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,19 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+
+# Include each railties manually, excluding `active_storage/engine`
+# Guide from here: https://mikerogers.io/2018/04/13/remove-activestorage-from-rails-5-2.html
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +24,7 @@ module Api
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    #added for rack-cors gem to make calls to localhost:3000
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
