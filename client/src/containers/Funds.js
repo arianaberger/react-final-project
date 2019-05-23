fd2f48b60902a4fae50069eb30701ba1415d9ac0
arianaberger <ariana.berger@gmail.com>
2019-05-23 17:55:46 -0400

    Add key to fund container

--- client/src/containers/Funds.js
+++ client/src/containers/Funds.js
@@ -4,7 +4,7 @@ import '../css/Funds.css';
 const Funds = (props) => (
   <div className="wrapper">
     {props.funds.map(fund =>
-      <div className='Fund'>
+      <div className='Fund' key={fund.id}>
         <h3>{fund.name}</h3>
         <h1>${fund.amount}</h1>
         Link to Edit
