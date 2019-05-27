24bd158163649568c6dc75979a035056ff4de53c
arianaberger <ariana.berger@gmail.com>
2019-05-27 11:19:43 -0400

    Work on account total model method

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,21 @@
 class Account < ApplicationRecord
   has_many :transactions
+
+  def self.account_total(account_id)
+    account = Account.find_by(id: account_id)
+    debits = []
+    credits = []
+
+    debits = account.transactions.each do |t|
+      if t.debit
+        debits.push(t.amount)
+      else
+        credits.push(t.amount)
+      end
+    end
+
+    account_total = debits.sum - credits.sum
+    return account_total
+  end
+
 end
