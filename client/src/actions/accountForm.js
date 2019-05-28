29e519875cb2d0f4048cef8a2812ce132b80beba
arianaberger <ariana.berger@gmail.com>
2019-05-27 21:13:13 -0400

    Create account form actions

---
+++ client/src/actions/accountForm.js
@@ -0,0 +1,12 @@
+export const updateAccountFormData = accountFormData => {
+  return {
+    type: 'UPDATED_DATA',
+    accountFormData
+  }
+}
+
+export const resetAccountForm = () => {
+  return (
+    type: 'RESET_ACCOUNT_FORM'
+  )
+}
