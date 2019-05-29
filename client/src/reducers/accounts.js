aca944ea9301919af88ca234d8e47d92a2f1893d
arianaberger <ariana.berger@gmail.com>
2019-05-28 21:51:33 -0400

    Need to finish updating reducer for update account

--- client/src/reducers/accounts.js
+++ client/src/reducers/accounts.js
@@ -1,7 +1,13 @@
-export default (state= [], action) => {
+export default (state = [], action) => {
   switch(action.type) {
     case 'GET_ACCOUNTS_SUCCESS':
       return action.accounts;
+
+      case 'UPDATE_ACCOUNT_SUCCESS':
+        return {
+          ...state,
+          account: [...state.accounts.filter(account )]
+          state.concat(action.account);
 
       default: return state;
   }
