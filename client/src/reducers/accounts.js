eaef99ecccc995101530ee7f68a51f15c1f1fe9f
arianaberger <ariana.berger@gmail.com>
2019-05-31 20:47:45 -0400

    Update account reducer so state includes accounts and transactions, currently getting error

--- client/src/reducers/accounts.js
+++ client/src/reducers/accounts.js
@@ -1,10 +1,22 @@
-export default (state = [], action) => {
+export default (state = {accounts: [],
+transactions: []}, action) => {
   switch(action.type) {
     case 'GET_ACCOUNTS_SUCCESS':
-      return action.accounts;
+      return {...state, accounts: action.accounts}
 
       case 'UPDATE_ACCOUNT_SUCCESS' :
-          console.log("state from accounts reducer:", action.account)
+        // initialStatestate.accounts.filter(account => account.id !== action.id)
+        // return {
+        //   ...new_state,
+        //   accounts: [
+        //     ...new_state.accounts, action.account
+        // ]}
+        return {
+          ...state,
+          accounts: [
+            ...state.accounts, action.account
+          ]}
+
 
       default: return state;
     }
