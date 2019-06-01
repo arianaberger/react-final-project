3d8c7e32df8fbc127b4d367f97b4db2c38d991c2
arianaberger <ariana.berger@gmail.com>
2019-06-01 15:36:19 -0400

    Get links working for transaction container

---
+++ client/src/components/ChooseTransactionComponent.js
@@ -0,0 +1,22 @@
+import { NavLink } from 'react-router-dom';
+
+import React, { Component } from 'react';
+
+class ChooseTransactionComponent extends Component {
+  render() {
+    return(
+      <>
+      <div className="AddTransaction">
+        <NavLink to="/debit" className="transaction-link">+ Add a Deposit</NavLink>
+      </div>
+      <div className="AddTransaction">
+        <NavLink to="/credit" className="transaction-link">
+          + Add a Payment
+        </NavLink>
+      </div>
+      </>
+    )
+  }
+}
+
+export default ChooseTransactionComponent
--- client/src/containers/AddTransactionContainer.js
+++ client/src/containers/AddTransactionContainer.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { NavLink,
   BrowserRouter as Router,
   Route, } from 'react-router-dom';
+import ChooseTransactionComponent from '../components/ChooseTransactionComponent'
 import AddTransactionComponent from './AddTransactionComponent'
-import '../css/AddTransaction.css';
-import './TransactionInput';
 
-class ChooseTransactionContainer extends Component {
+import '../css/AddTransaction.css';
+
+class AddTransactionContainer extends Component {
   render() {
     return(
-      <div className="AddTransaction">
         <Router>
+          <ChooseTransactionComponent />
           <Route
             exact path="/debit"
             component={AddTransactionComponent} />
@@ -18,13 +19,8 @@ class ChooseTransactionContainer extends Component {
             exact path="/credit"
             component={AddTransactionComponent} />
         </Router>
-      </div>
-
-
-
-
     )
   }
 }
 
-export default ChooseTransactionContainer
+export default AddTransactionContainer
