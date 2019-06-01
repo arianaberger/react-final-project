685017b2c0d430c1ed706cece406565c2a6488ba
arianaberger <ariana.berger@gmail.com>
2019-05-31 21:24:46 -0400

    Still get error for reducer when updating an account name

--- client/src/reducers/accounts.js
+++ client/src/reducers/accounts.js
@@ -2,22 +2,21 @@ export default (state = {accounts: [],
 transactions: []}, action) => {
   switch(action.type) {
     case 'GET_ACCOUNTS_SUCCESS':
-      // return {...state, accounts: action.accounts}
       return {...state, accounts: action.accounts}
 
       case 'UPDATE_ACCOUNT_SUCCESS' :
-        // initialStatestate.accounts.filter(account => account.id !== action.id)
-        // return {
-        //   ...new_state,
-        //   accounts: [
-        //     ...new_state.accounts, action.account
-        // ]}
+        const new_state = () => {
+          return {...state,
+          accounts: [...state.accounts.filter(account => account.id !== action.account.id)]
+          }
+          debugger
+        }
         return {
-          ...state,
+          ...new_state,
           accounts: [
-            ...state.accounts, action.account
-          ]}
-
+            ...new_state.accounts, action.account
+          ]
+        }
 
       default: return state;
     }
