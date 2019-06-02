e87221af2ab15cd8a604ccbdf86aac3d383eace6
arianaberger <ariana.berger@gmail.com>
2019-06-01 23:10:30 -0400

    Add event handlers to try and show transaction form

--- NOTES.md
+++ NOTES.md
@@ -1,14 +1,7 @@
 1. reset account form is not working in actions/accounts.js line 39
 2. TransactionInput is not getting props???
+3. route keeps adding onto the ned of url!!!! gahhh
 
-
-Import provider to index.js
-create a store.js and import it into index.js
-https://github.com/BekahHW/Women-of-Trauma-Client/blob/master/src/store.js
-
-Add components to router with links
-Will link to another container that uses componentDidMount to get api data (usingmapstate to props)
-This is handed down to the component
 
 
 
--- client/src/containers/AddTransactionContainer.js
+++ client/src/containers/AddTransactionContainer.js
@@ -2,23 +2,58 @@ import React, { Component } from 'react';
 import { NavLink,
   BrowserRouter as Router,
   Route, } from 'react-router-dom';
-import ChooseTransactionComponent from '../components/ChooseTransactionComponent'
+// import ChooseTransactionComponent from '../components/ChooseTransactionComponent'
 import AddTransactionComponent from './AddTransactionComponent'
 import '../css/AddTransaction.css';
 
 class AddTransactionContainer extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      show_transaction_form: false,
+      debit_form: true
+    }
+    this.handleClickDebit = this.handleClickDebit.bind(this);
+    this.handleClickCredit = this.handleClickCredit.bind(this);
+  }
+
+  handleClickDebit(event) {
+    this.setState({
+      debit_form: true,
+      show_transaction_form: true
+    })
+  }
+
+  handleClickCredit(event) {
+    this.setState({
+      debit_form: false,
+      show_transaction_form: true
+    })
+  }
+
   render() {
     return(
-        <Router>
-          <ChooseTransactionComponent />
-          <Route
-            exact path="/debit"
-            component={AddTransactionComponent} />
-          <Route
-            exact path="/credit"
-            component={AddTransactionComponent} />
-        </Router>
+      <>
+        <button onClick={this.handleClickDebit}>+ Add a Deposit</button>
+        <button onClick={this.handleClickCredit}>+ Add a Payment</button>
+      </>
     )
+    if (this.state.show_transaction_form) {
+      return (
+        "show the form!"
+      )
+    }
+
+        // <Router>
+        //   <ChooseTransactionComponent />
+        //   <Route
+        //     exact path="addtransaction/debit"
+        //     component={AddTransactionComponent} />
+        //   <Route
+        //     exact path="addtransaction/credit"
+        //     component={AddTransactionComponent} />
+        // </Router>
   }
 }
 
