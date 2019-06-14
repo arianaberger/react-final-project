841633634f1cc2b97472c153221d907416b9e32a
arianaberger <ariana.berger@gmail.com>
2019-06-13 22:00:36 -0400

    Store all form data in local state for debit form

--- client/src/components/DebitFirstInput.js
+++ client/src/components/DebitFirstInput.js
@@ -8,48 +8,47 @@ const DebitFirstInput = (props) => {
   const {amount, counterparty, date} = props.state
 
   return(
-  <div>
-  <Form onSubmit={props.handleFirstSubmit}>
+    <div>
+    <Form onSubmit={props.handleFirstSubmit}>
 
-    <Form.Group controlId="formBasicAmount">
-      <Form.Label>Amount:</Form.Label>
-      <Form.Control
-        type="text"
-        name="amount"
-        value={amount}
-        onChange={props.handleOnChange}
-       />
-    </Form.Group>
+      <Form.Group controlId="formBasicAmount">
+        <Form.Label>Amount:</Form.Label>
+        <Form.Control
+          type="text"
+          name="amount"
+          value={amount}
+          onChange={props.handleOnChange}
+         />
+      </Form.Group>
 
-    <Form.Group controlId="formBasicInstitution">
-      <Form.Label>Institution:</Form.Label>
-      <Form.Control
-        type="text"
-        name="counterparty"
-        value={counterparty}
-        onChange={props.handleOnChange}
-      />
-    </Form.Group>
+      <Form.Group controlId="formBasicInstitution">
+        <Form.Label>Institution:</Form.Label>
+        <Form.Control
+          type="text"
+          name="counterparty"
+          value={counterparty}
+          onChange={props.handleOnChange}
+        />
+      </Form.Group>
 
-    <Form.Group controlId="formBasicDate">
-      <Form.Label>Date:</Form.Label>
-      <DatePicker
-        name="date"
-        selected={date}
-        onChange={props.handleDateChange}
-        placeholderText="Click to select a date"
-        dateFormat="d MMMM, yyyy"
-      />
-    </Form.Group>
+      <Form.Group controlId="formBasicDate">
+        <Form.Label>Date:</Form.Label>
+        <DatePicker
+          name="date"
+          selected={date}
+          onChange={props.handleDateChange}
+          placeholderText="Click to select a date"
+          dateFormat="MMMM d, yyyy"
+        />
+      </Form.Group>
 
 
-    <Button variant="dark" type="submit">
-      Add Deposit
-    </Button>
-  </Form>
-  </div>
-)
-
+      <Button variant="dark" type="submit">
+        Add Deposit
+      </Button>
+    </Form>
+    </div>
+  )
 }
 
 export default DebitFirstInput
--- client/src/containers/DebitContainer.js
+++ client/src/containers/DebitContainer.js
@@ -4,7 +4,6 @@ import { Redirect } from 'react-router-dom';
 import { getAccounts} from '../actions/accounts';
 import DebitFirstInput from '../components/DebitFirstInput'
 import DebitSecondInput from '../components/DebitSecondInput'
-import { updateTransactionFormData, resetTransactionForm } from '../actions/transactionForm'
 import { createTransaction } from '../actions/transactions'
 
 class DebitContainer extends Component {
@@ -14,6 +13,7 @@ class DebitContainer extends Component {
     this.state = {
       first_submit: false,
       authenticate: false,
+
       //Initial state for form
       amount: '',
       counterparty: '',
@@ -26,68 +26,67 @@ class DebitContainer extends Component {
     this.handleDateChange = this.handleDateChange.bind(this);
   }
 
-
-  componentDidMount(){
-    this.props.getAccounts()
-    this.props.resetTransactionForm()
-  }
-
-  handleOnChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-
-    const currentTransactionFormData = Object.assign({}, this.props.transactionFormData, {
-      [event.target.name]: event.target.value
-    });
-    this.props.updateTransactionFormData(currentTransactionFormData);
-  }
-
-  handleDateChange(date) {
-    this.setState({
-      date: date
-    })
-  }
-
-  handleFirstSubmit = event => {
-    event.preventDefault();
-    this.props.createTransaction(this.props.transactionFormData)
-    this.setState({ first_submit: true })
-  }
-
-  handleSecondSubmit = event => {
-    event.preventDefault();
-    this.props.createTransaction(this.props.transactionFormData)
-    this.props.resetTransactionForm()
-    this.setState({ first_submit: false, authenticate: true })
-  }
-
-  render() {
-    //Redirect to accounts page when form is submitted
-    if (this.state.authenticate === true) {
-      return <Redirect to='/' />
+    componentDidMount(){
+      this.props.getAccounts()
     }
 
-    return(
-      <>
-        {this.state.first_submit ? null : <DebitFirstInput state={this.state} handleDateChange={this.handleDateChange} transactionFormData={this.props.transactionFormData} handleOnChange={this.handleOnChange} handleFirstSubmit={this.handleFirstSubmit} />}
-        {this.state.first_submit ? <DebitSecondInput accounts={this.props.accounts} transactionFormData={this.props.transactionFormData} handleOnChange={this.handleOnChange} handleSecondSubmit={this.handleSecondSubmit}/> : null}
-      </>
-    )
+    handleOnChange = event => {
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    }
+
+    handleDateChange(date) {
+      this.setState({
+        date: date
+      })
+    }
+
+    handleFirstSubmit = event => {
+      event.preventDefault();
+      this.props.createTransaction(this.state)
+      this.setState({ first_submit: true })
+    }
+
+    handleSecondSubmit = event => {
+      event.preventDefault();
+      this.props.createTransaction(this.state)
+      this.setState({
+        first_submit: false,
+        authenticate: true,
+        amount: '',
+        counterparty: '',
+        date: new Date(),
+        account_id: 1,
+        parent_id: '',
+        debit: true,
+        percentage: '',
+     })
+    }
+
+    render() {
+      //Redirect to accounts page when form is submitted
+      if (this.state.authenticate === true) {
+        return <Redirect to='/' />
+      }
+
+      return(
+        <>
+          {this.state.first_submit ? null : <DebitFirstInput state={this.state} handleDateChange={this.handleDateChange} transactionFormData={this.props.transactionFormData} handleOnChange={this.handleOnChange} handleFirstSubmit={this.handleFirstSubmit} />}
+          {this.state.first_submit ? <DebitSecondInput accounts={this.props.accounts} transactionFormData={this.props.transactionFormData} handleOnChange={this.handleOnChange} handleSecondSubmit={this.handleSecondSubmit}/> : null}
+        </>
+      )
+    }
   }
-}
 
 const mapStateToProps = (state) => {
   return {
     //is accounts.accounts. not considered best practice? Or is this okay?
     accounts: state.accounts.accounts,
-    transactionFormData: state.transactionFormData
   }
 }
 
 export default connect(mapStateToProps, {
   getAccounts,
-  updateTransactionFormData,
   createTransaction,
-  resetTransactionForm
 })(DebitContainer)
