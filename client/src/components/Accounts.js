353e13b7531cd0a3a0a75a6ed0d32ddb0075407c
arianaberger <ariana.berger@gmail.com>
2019-06-15 16:15:17 -0400

    Add edit icon to accounts page

--- client/src/components/Accounts.js
+++ client/src/components/Accounts.js
@@ -2,20 +2,22 @@ import React from 'react';
 import '../css/Accounts.css';
 import { NavLink } from 'react-router-dom';
 import AccountForm from '../containers/AccountForm'
-
+import format from 'accounting-js';
+import edit from '../images/edit.png'
 
 const Accounts = (props) => (
   <div className="AccountWrapper">
     {props.accounts.map(account =>
       <div className='Accounts' key={account.id}>
-        <div className="AccountBuffer"></div>
         <div className="AccountName">
           <NavLink to={`/accounts/${account.id}`}><h3>{account.name}</h3></NavLink>
         </div>
         <div className="AccountTotal">
-          <h1>${account.account_total}</h1>
+          <h2>{format.formatMoney(account.account_total)}</h2>
         </div>
-        <div className="AccountBuffer"></div>
+        <div className="AccountEdit">
+          {account.id != 1 ? <img src={edit} alt="edit"/> : null}
+        </div>
         {props.update ? <AccountForm accountId={account.id} /> : null}
       </div>
     )}
