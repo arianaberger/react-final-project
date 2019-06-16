7035dd77b0a185e832553ab623bef9f6374fc1c8
arianaberger <ariana.berger@gmail.com>
2019-06-16 18:13:12 -0400

    Add more CSS to account and make credits red

--- client/src/components/TransactionsContainer.js
+++ client/src/components/TransactionsContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/Accounts.css';
 import {Button} from 'react-bootstrap';
+import format from 'accounting-js';
 
 const TransactionsContainer = (props) => (
   <div className="wrapper">
@@ -10,14 +11,18 @@ const TransactionsContainer = (props) => (
         <h5>{transaction.counterparty}</h5>
         <p>{transaction.date}</p>
       </div>
+
       <div className="Transaction2">
-        <h5>${transaction.amount}</h5>
+      {transaction.debit ?
+        <h5>{format.formatMoney(transaction.amount)}</h5> :
+        <h5 className="credit">{format.formatMoney(transaction.amount)}</h5>}
       </div>
+
       <div className="Transaction3">
-        {props.account_id === 1 ? null : <Button variant="outline-secondary" size="sm" onClick={() => props.onDeleteClick(transaction)}>x</Button>}
+        {props.account_id === 1 ? null : <Button variant="outline-secondary" size="sm" onClick={() => props.onDeleteClick(transaction)}>X</Button>}
       </div>
     </div>
-    )}
+  )}
   </div>
 )
 
